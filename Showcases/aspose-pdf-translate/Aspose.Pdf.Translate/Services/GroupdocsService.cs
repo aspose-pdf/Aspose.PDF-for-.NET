fed163f8a76d8a75ba7328389b65277440893ee1
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2024-02-05 17:45:35 +0200

    groupdocs flow

--- Showcases/aspose-pdf-translate/Aspose.Pdf.Translate/Services/GroupdocsService.cs
+++ Showcases/aspose-pdf-translate/Aspose.Pdf.Translate/Services/GroupdocsService.cs
@@ -1,5 +1,4 @@
-﻿using Aspose.Pdf.Translate.Helper;
-using Aspose.Pdf.Translate.Services.Interface;
+﻿using Aspose.Pdf.Translate.Services.Interface;
 using GroupDocs.Translation.Cloud.Sdk.Api;
 using GroupDocs.Translation.Cloud.Sdk.Client;
 using GroupDocs.Translation.Cloud.Sdk.Model;
@@ -17,6 +16,7 @@ namespace Aspose.Pdf.Translate.Services
             this.logger = logger;
             conf = new Configuration
             {
+                OAuthFlow = GroupDocs.Translation.Cloud.Sdk.Client.Auth.OAuthFlow.APPLICATION,
                 OAuthClientId = Environment.GetEnvironmentVariable("Translate_ClientId"),
                 OAuthClientSecret = Environment.GetEnvironmentVariable("Translate_ClientSecret")
             };
@@ -24,53 +24,92 @@ namespace Aspose.Pdf.Translate.Services
 
         public async Task<Stream> TranslateDocument(string documentId, byte[] fileData, string inputType, string from, string to, string fileName)
         {
-            try
+            var instance = new TranslationApi(conf);
+            var fileApi = new FileApi(conf);
+
+            var url = await fileApi.FileUploadPostAsync(
+                FileTypeToFormat(inputType).ToString().ToLowerInvariant(),
+                new MemoryStream(fileData));
+
+            FileRequest autoPostRequest = new FileRequest()
             {
-                var token = await new TokenRetriever(conf.OAuthClientId, conf.OAuthClientSecret).GetTokenAsync();
+                SourceLanguage = from,
+                TargetLanguages = new List<string> { to },
+                Format = FileRequest.FormatEnum.Pdf,
+                OutputFormat = FileTypeToFormat(inputType).ToString().ToLowerInvariant(),
+                OriginalFileName = fileName,
+                Url = url
+            };
+            var response = instance.AutoPost(autoPostRequest);
 
-                //await new FileUpload(token).SendFileRequest("Pdf", new MemoryStream(fileData), "1.pdf");
-
-                var t = new TranslationRequest
+            for (int i = 0; i < 100; i++)
+            {
+                var status = instance.DocumentRequestIdGet(response.Id);
+                if (status.Urls.Count > 0)
                 {
-                    File = "1.pdf",
-                    Format = "pdf",
-                    OutputFormat = "pdf",
-                    SourceLanguage = "en",
-                    TargetLanguages = new List<string>() { "fr" },
-                    OriginalFileName = "1.pdf",
-                    
-                };
-
-                var res = await new TranslationUpload(token).SendTranslationRequest(t);
-
-                return null;
+                    var client = new HttpClient();
+                    return await client.GetStreamAsync(status.Urls.First().Key);
+                }
+                await Task.Delay(1000);
             }
-            catch (Exception ex)
-            {
-                logger.LogError(ex.Message, ex);
-                return null;
-            }
+
+            throw new Exception("Timout for translate id=" + response.Id);
         }
-
-        private Dictionary<string, string[]> SupportedTranslateLanguages
-        { get; }
-        = new Dictionary<string, string[]>
-        {
-            { "english", new [] { "french", "german", "italian", "spanish", "portuguese", "polish", "russian", "arabic", "chinese", "lithuanian", "latvian", "azerbaijani" } },
-            { "french", new [] { "english", "german", "italian" } },
-            { "german", new [] { "english", "french" } },
-            { "italian", new [] { "english", "french" } },
-            { "spanish", new [] { "english" } },
-            { "portuguese", new [] { "english" } },
-            { "polish", new [] { "english" } },
-            { "russian", new [] { "english" } },
-            { "arabic", new [] { "english" } },
-            { "chinese", new [] { "english" } }
-        };
 
         public string GetSafeInputName(string folderName, string fileName)
         {
             return HttpUtility.UrlEncode(folderName.Substring(0, 4) + Path.GetFileNameWithoutExtension(fileName).Substring(0, Path.GetFileNameWithoutExtension(fileName).Length > 8 ? 8 : Path.GetFileNameWithoutExtension(fileName).Length) + Path.GetExtension(fileName));
+        }
+
+        private FileRequest.FormatEnum FileTypeToFormat(string fileType)
+        {
+            switch(fileType?.ToLowerInvariant())
+            {
+                case "html":
+                    return FileRequest.FormatEnum.Html;
+                case "ppt":
+                    return FileRequest.FormatEnum.Ppt;
+                case "pptx":
+                    return FileRequest.FormatEnum.Pptx;
+                case "pptm":
+                    return FileRequest.FormatEnum.Pptm;
+                case "txt":
+                    return FileRequest.FormatEnum.Txt;
+                case "csv":
+                    return FileRequest.FormatEnum.Csv;
+                case "doc":
+                    return FileRequest.FormatEnum.Doc;
+                case "docm":
+                    return FileRequest.FormatEnum.Docm;
+                case "docx":
+                    return FileRequest.FormatEnum.Docx;
+                case "md":
+                    return FileRequest.FormatEnum.Md;
+                case "odp":
+                    return FileRequest.FormatEnum.Odp;
+                case "ods":
+                    return FileRequest.FormatEnum.Ods;
+                case "odt":
+                    return FileRequest.FormatEnum.Odt;
+                case "pdf":
+                    return FileRequest.FormatEnum.Pdf;
+                case "resx":
+                    return FileRequest.FormatEnum.Resx;
+                case "rtf":
+                    return FileRequest.FormatEnum.Rtf;
+                case "srt":
+                    return FileRequest.FormatEnum.Srt;
+                case "tsv":
+                    return FileRequest.FormatEnum.Tsv;
+                case "xls":
+                    return FileRequest.FormatEnum.Xls;
+                case "xlsm":
+                    return FileRequest.FormatEnum.Xlsm;
+                case "xlsx":
+                    return FileRequest.FormatEnum.Xlsx;
+                default:
+                    throw new NotImplementedException(fileType);
+            }
         }
     }
 }
